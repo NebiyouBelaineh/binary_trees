df8b1d8dea81ee13ccfd8409a63943464c284cb0
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-01-31 01:03:30 +0300

    Add NULL condition for task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,11 +11,11 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int balance_factor;
 
-	if (!tree)
-		return (0);
-
 	size_t hL = tree_height(tree->left);
 	size_t hR = tree_height(tree->right);
+
+	if (!tree)
+		return (0);
 
 	/**
 	* finds the balance factor for the binary tree
