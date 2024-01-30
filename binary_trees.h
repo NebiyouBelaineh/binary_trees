fa3915800c58e812a5ea14a37f8bdc88428117f8
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-01-30 20:53:27 +0300

    Add solution for task 6

--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,6 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 /* checks if a given node is a root */
 int binary_tree_is_root(const binary_tree_t *node);
+/* Traveres through a tree in pre-order traversal */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 #endif /*BINARY_TREES_H*/
