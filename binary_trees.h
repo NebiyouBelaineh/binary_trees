eadec0c67cec16701b0b57c4adfaff90fc87b03e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-01-30 21:16:42 +0300

    Add solution for task 8

--- binary_trees.h
+++ binary_trees.h
@@ -45,4 +45,6 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 /* Traveres through a tree in pre-order traversal */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+/* Traveres through a tree in post-order traversal */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 #endif /*BINARY_TREES_H*/
