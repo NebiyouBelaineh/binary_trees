089c3aee17c168db892618ab163ab6508d2def9d
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-01-31 15:52:29 +0300

    Change perfect tree verifying method

--- binary_trees.h
+++ binary_trees.h
@@ -66,4 +66,6 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 /*checks if a binary tree is perfect*/
 int binary_tree_is_perfect(const binary_tree_t *tree);
+/* finds the sibling of a node */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif /*BINARY_TREES_H*/
