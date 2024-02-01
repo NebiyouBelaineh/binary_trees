f9001df132956fd662612bf18a5813daa715df9d
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-01 03:47:57 +0100

    task23 done right rotate

--- binary_trees.h
+++ binary_trees.h
@@ -112,6 +112,8 @@ binary_tree_t *dequeue(queue_t *queue);
 int binary_tree_is_complete(const binary_tree_t *tree);
 /* performs a left-rotation on a binary tree */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+/* performs a right-rotation on a binary tree */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 /* checks if a binary tree is a BST */
 int binary_tree_is_bst(const binary_tree_t *tree);
 /* inserts a value in a Binary Search Tree */
